9369a53e07fa0884f41e3054e56d8a870a70333c
root <root@DESKTOP-482253N.localdomain>
2021-10-17 12:55:49 +1100

    assignment passed

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,25 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    # Create Stubs
+    @recipe.ingredients.build(name: "First Ingredient")
+    @recipe.ingredients.build(name: "Second Ingredient")
   end
 
   def create
+    Recipe.create(recipe_params)
+    redirect_to recipes_path
   end
+
+  # def title=(title)
+  # self.title = params([:recipe][:title])
+  # end
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title, 
+      ingredients_attributes: [:name, :quantity]
+    )
+  end
+
 end
